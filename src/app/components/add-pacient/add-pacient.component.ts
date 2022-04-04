841eada1de755ceb913f3865d68be9ab8cb2c2f3
DianaGradinaru <diana.gradinaru.sincai@gmail.com>
2022-04-04 20:24:41 +0300

    data validations for add pacient form

--- src/app/components/add-pacient/add-pacient.component.ts
+++ src/app/components/add-pacient/add-pacient.component.ts
@@ -1,17 +1,35 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-add-pacient',
   templateUrl: './add-pacient.component.html',
-  styleUrls: ['./add-pacient.component.css']
+  styleUrls: ['./add-pacient.component.css'],
 })
 export class AddPacientComponent implements OnInit {
+  nume!: string;
+  prenume!: string;
+  dataNasterii!: Date;
+  sex!: string;
+  cnp!: number;
+  telefon!: number;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  onSubmit() {
+    if (!this.nume || !this.prenume || !this.dataNasterii || !this.sex) {
+      alert('Campurile Nume, Prenume, Data Nașterii, Sex sunt obligatorii');
+    }
+    if (this.cnp && this.cnp.toString().length != 13) {
+      alert('Campul CNP trebuie sa aiba 13 caractere numerice');
+    }
+    const dateDifferenceinDays = moment().diff(
+      moment(this.dataNasterii.valueOf()),
+      'days'
+    );
+    if (dateDifferenceinDays <= 0) {
+      alert('Data nasterii trebuie sa fie in trecut');
+    }
   }
-
 }
-
